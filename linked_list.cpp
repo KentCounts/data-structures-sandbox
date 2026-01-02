2f0437d83da66a4d8cc8d44ceeb22006ba64990c
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-01-02 13:19:01 -0600

    init branch and outline linked list

---
+++ linked_list.cpp
@@ -0,0 +1,50 @@
+// list node
+// has value
+// next node
+
+// list struct
+// has head
+// size node #
+
+// init function
+// define head
+// define size
+
+// is empty?
+// head null
+
+// push front
+// new node
+// new head
+// link new head to old head
+// size +1
+
+// push back
+// new node
+// no next
+// if else for head
+// define next 
+// size +1
+
+// pop front
+// if empty
+// remove head
+// new head = next
+// size -1
+
+// pop back
+// if empty
+// if singular
+// remove last
+// size -1
+
+
+
+// other ideas
+
+// search
+// insert
+// remove
+// clear
+// size
+// traverse
