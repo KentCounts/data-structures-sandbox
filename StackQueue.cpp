dff170038f1b3926d23bdebab7bea47b0b962b0e
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-01-19 18:12:29 -0600

    comment outlines for stack and queue

---
+++ StackQueue.cpp
@@ -0,0 +1,24 @@
+// stack ideas
+
+// stack means = last in first out
+
+
+// push
+// pop (edge case: empty)
+// peek (edge case: empty)
+// empty
+// size
+// clear
+
+
+// queue ideas
+
+
+// Q means = first in first out
+
+// add to back
+// pop from front (edge case empty)
+// peek front (edge case empty)
+// empty
+// size
+// clear
