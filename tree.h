374a8baf5f45cde237fdae42cb90d1f5f5e5cbdf
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-02-10 14:05:35 -0600

    outline tree.h for later building of tree.cpp

--- tree.h
+++ tree.h
@@ -1,6 +1,36 @@
 #pragma once
 
+// tree node structure
+// data
+// left branch
+// right branch
 
-// BFS
+// construct a node
 
-// DFS
+// tree root
+
+// tree size
+
+// handle duplicats?
+
+// insert (recursive?)
+
+// remove (recursive?)
+
+// minimum node
+
+// depth first search
+// bredth first search
+
+// clear tree
+// deep copy tree
+
+// constructors
+// destructors
+
+// print in order
+// pre order?
+// post order?
+// is tree empty
+// get tree size
+// initialize with sample data
