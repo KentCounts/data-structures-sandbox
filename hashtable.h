973cf38e1adc66c433da0709dff2b81393910a4b
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-02-19 10:33:52 -0600

    new branch for hash and .h outlining for hash

--- hashtable.h
+++ hashtable.h
@@ -1,1 +1,35 @@
 #pragma once
+
+// node struct
+// key
+// value
+
+// node table
+// capacity
+// size
+
+// hashing
+
+// find node
+// clear bucket
+// copy bucket
+// resize
+// rehash
+
+
+// constructors and destructors (rule of 3 maybe)
+
+// insert
+// remove
+// contains
+// get
+
+// is empty
+// get size
+// get capacity
+// load factor
+
+// clear
+// init sample
+// print all
+// print bucket
