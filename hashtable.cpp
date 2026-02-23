03c1103847ae8509f30c7fc2edb0f4dcb57429bb
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-02-23 14:11:22 -0600

    basic find functions for node lookup

--- hashtable.cpp
+++ hashtable.cpp
@@ -8,3 +8,24 @@ std::size_t HashTable::hash(int key) const
         % static_cast<int>(capacity)
         );
 }
+
+HashTable::Node* HashTable::findNodeInBucket(std::size_t idx, int key) const
+{
+    Node* current = table[idx];
+
+    while (current != nullptr)
+    {
+        if (current->key == key)
+            return current;
+
+        current = current->next;
+    }
+
+    return nullptr;
+}
+
+HashTable::Node* HashTable::findNode(int key) const
+{
+    std::size_t idx = hash(key);
+    return findNodeInBucket(idx, key);
+}
