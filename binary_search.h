af3899e7689092b87b37ba898da5e506816ce597
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-03-03 13:39:56 -0600

    added binary_search.cpp for later development

--- binary_search.h
+++ binary_search.h
@@ -1,1 +1,14 @@
 #pragma once
+
+// requires input to be sorted
+
+// recursive search
+
+// basic binary iterative search
+
+
+// duplicate aware search?
+
+// lower + upper bounds
+
+// check that input is sorted before begin
