e48391ec9966d555a96977edb1013c5a3022b1d8
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-03-19 11:31:36 -0500

    sort and is sorted check

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,10 +1,27 @@
 #include "merge_sort.h"
 #include <iostream>
 
-// sort
+void MergeSort::Sort(int arr[], std::size_t size)
+{
+    if (arr == nullptr || size <= 1)
+        return;
 
-// is sorted
+    mergeSort(arr, 0, static_cast<int>(size) - 1);
+}
 
+bool MergeSort::IsSorted(const int arr[], std::size_t size)
+{
+    if (arr == nullptr || size <= 1)
+        return true;
+
+    for (std::size_t i = 1; i < size; ++i)
+    {
+        if (arr[i] < arr[i - 1])
+            return false;
+    }
+
+    return true;
+}
 // mergesort
 
 // merge
