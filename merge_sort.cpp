7a90d04747d66b0787894738a179dac6e2bc1138
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-03-20 09:32:21 -0500

    mergesort and merge step

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -22,6 +22,56 @@ bool MergeSort::IsSorted(const int arr[], std::size_t size)
 
     return true;
 }
-// mergesort
 
-// merge
+void MergeSort::mergeSort(int arr[], int left, int right)
+{
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+
+    mergeSort(arr, left, mid);
+    mergeSort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+void MergeSort::merge(int arr[], int left, int mid, int right)
+{
+    int n = right - left + 1;
+
+    int* temp = new int[n];
+
+    int i = left;      // pointer for left
+    int j = mid + 1;   // pointer for right
+    int k = 0;         // pointer for temp array
+
+    while (i <= mid && j <= right)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp[k++] = arr[i++];
+        }
+        else
+        {
+            temp[k++] = arr[j++];
+        }
+    }
+
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+
+    while (j <= right)
+    {
+        temp[k++] = arr[j++];
+    }
+
+    for (int t = 0; t < n; ++t)
+    {
+        arr[left + t] = temp[t];
+    }
+
+    delete[] temp;
+}
