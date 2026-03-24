55c092f1ae36aa9264a3a752b1a5e0dbfa87ac9f
KentCounts <77399771+KentCounts@users.noreply.github.com>
2026-03-24 08:00:33 -0500

    build and outline .cpp

---
+++ quick_sort.cpp
@@ -0,0 +1,23 @@
+#include "quick_sort.h"
+#include <utility>
+
+
+void QuickSort::Sort(int arr[], std::size_t size)
+{
+    // no logic yet
+}
+
+bool QuickSort::IsSorted(const int arr[], std::size_t size)
+{
+    // no logic yet
+}
+
+void QuickSort::quickSort(int arr[], int left, int right)
+{
+    // no logic yet
+}
+
+int QuickSort::partition(int arr[], int left, int right)
+{
+    // no logic yet
+}
